a72c92b4a1fe47c07e194c9937656cbf1cecca8b
OlesiaPastushenko <olesya.pastushenko1@gmail.com>
2023-02-02 16:49:08 +0100

    cards styles changed

--- src/components/cards/LangCard.jsx
+++ src/components/cards/LangCard.jsx
@@ -2,7 +2,7 @@ import "./SkillCardStyle.css"
 
 export function LangCard({name, imgSrc, level}) {
   return (
-    <div className="skillCard">
+    <div className="skillCardLang">
         <img className="imgLang" src={imgSrc} alt={name+"logo"}></img>
         <div className='cardText'>{name}</div>
         <div className="level">{level}</div>
