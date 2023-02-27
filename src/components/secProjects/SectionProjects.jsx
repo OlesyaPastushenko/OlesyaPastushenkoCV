fce42b67458d95e0da975e27693ae3e711f7fea9
OlesiaPastushenko <olesya.pastushenko1@gmail.com>
2023-02-27 14:26:58 +0100

    new project added

--- src/components/secProjects/SectionProjects.jsx
+++ src/components/secProjects/SectionProjects.jsx
@@ -4,6 +4,11 @@ import "./secProjects.css"
 export function SectionProjects() {
   const [display1, setDisplay1] = useState(false)
   const [display2, setDisplay2] = useState(false)
+  const [display3, setDisplay3] = useState(false)
+
+  const onClickDisplay3 =()=> {
+    setDisplay3(!display3)
+  } 
   const onClickDisplay1 =()=> {
     setDisplay1(!display1)
   }
@@ -44,6 +49,30 @@ export function SectionProjects() {
             <a href="https://graceful-mandazi-47eeac.netlify.app/">
             <button className='whiteBtn'>View at Netlify</button></a>
         </div>
+        <div className="wrapSubAndBtn">
+        <div className='subTitle'>Test task for another company<p>React JS/API NovaPoshta/Axios/LocalStorage</p></div>
+        <button className="proBtn" onClick={onClickDisplay3}>{ display3 ? "Hide Details" : "Show details"}</button>
+        </div>
+        <div className='prWrap'>
+            {display3 && 
+            <div className='description'> 
+            <p>1. The user enters the TTN number or his invoice number in the input field on the page.</p>
+            <p>2. The app sends a request for current data to the goods place of delivery.</p>
+            <p>3. We display information about sending to the user.</p>
+            <p>4. The user must see all the previous TTN numbers in the list in the Sitbar. Even after the page reboot.</p>
+            <p>5. The click on the already added TTN number must automatically be a request for the status of finding the goods (delivery) and the relevant information is displayed. The TTN number should be automatically displayed in the input field.</p>
+            <p>6. The TTN invoice number must be verified.</p>
+            <p>7. There should be an opportunity to clean all the old TTN.</p>
+            <p>8. If you are incorrect, after clicking on the TTN Delivery Status button, a hint must be shown and the request should not be sent.</p>
+            <p>9. Going to the second tab, the user should be able to see the list of new mail branches, which is why you need to make a corresponding request to the API of the New Mail and display them with a list at your discretion.</p>
+            </div> 
+            }                           
+            <a href='https://github.com/OlesyaPastushenko/NovaPoshtaTestTask'>
+            <button className='blackBtn'>View at GitHub</button></a>
+            <a href="https://nova-poshta-test-task.vercel.app/">
+            <button className='whiteBtn'>View at Vercel</button></a>
+            </div>
+
         <div className="wrapSubAndBtn">
         <div className='subTitle'>Website for Real Estate Agency<p>React JS</p></div>
         <button className="proBtn" onClick={onClickDisplay2}>{ display2 ? "Hide Details" : "Show details"}</button>
